3875918ab975be5b22afcc487863c14ed3e92508
iluvcasca <sntcillian@gmail.com>
2024-01-31 15:37:51 +0000

    Change atoi condition
    Current overflow condition for ft_printf atoi was straight up bad...
    Changed it to match ft_atoi_safe function of mylibc, I hope that this time I got right

--- ft_printf_lexer_utils.c
+++ ft_printf_lexer_utils.c
@@ -6,7 +6,7 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 15:25:53 by kgriset           #+#    #+#             */
-/*   Updated: 2023/12/29 16:54:01 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/01/31 15:35:00 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,20 +51,20 @@ void	lexer_atoi(char **format, t_lexer_status *lexer_status, int *value,
 	while (lexer_status->lexer_state == state_map.current_state)
 	{
 		current_char = peek(*format, lexer_status->lexer_flags.i);
-		if (ft_isdigit(current_char) && (*value) >= INT_MAX / 10 && current_char
-			- '0' >= 8)
+        if (ft_isdigit(current_char) && (((*value) == INT_MAX / 10 
+            && current_char - '0' > 7 ) || ((*value) > INT_MAX / 10)))
 		{
 			(*value) = 0;
 			lexer_status->lexer_state = state_map.next_state;
 			lexer_status->return_value = -1;
 			(lexer_status->lexer_flags.i)++;
 		}
-		else if (ft_isdigit(current_char))
+        else if (ft_isdigit(current_char))
 		{
 			(*value) = (*value) * 10 + current_char - '0';
 			(lexer_status->lexer_flags.i)++;
 		}
-		else
+        else
 			lexer_status->lexer_state = state_map.next_state;
 	}
 }
